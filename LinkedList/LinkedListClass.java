f300db7248f965819acf230befac40db7a89f0d9
abhishekkumar085 <abhishek950820@gmail.com>
2025-03-19 16:05:10 +0530

    Added LL Codes

--- LinkedList/LinkedListClass.java
+++ LinkedList/LinkedListClass.java
@@ -27,7 +27,6 @@ public class LinkedListClass {
             mover = temp;
 
         }
-
         return head;
     }
 
@@ -42,17 +41,67 @@ public class LinkedListClass {
 
     }
 
+    private static int printLengthOftheLL(Node head) {
+        int cnt = 0;
+
+        Node temp = head;
+
+        while (temp != null) {
+            cnt++;
+            temp = temp.next;
+        }
+
+        return cnt;
+    }
+
+    private static boolean searchInLinkedList(Node head, int val) {
+
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == val) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    private static int[] twoSum(int arr[], int target) {
+        int n = arr.length;
+
+        int j = n - 1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] + arr[j] == target) {
+                return new int[] { i, j };
+            }
+
+        }
+
+        return new int[] { -1, -1 };
+    }
+
     public static void main(String[] args) {
         System.out.println("Hii Linked List");
 
-        int[] arr = { 12, 5, 6, 8 };
+        int[] arr = { 12, 5, 6, 8, 5, 4 };
         Node y = new Node(arr[2]);
 
         Node head = convertArr2LL(arr);
         // System.out.println(head.data);
         // System.out.println(y.next);
+        // System.out.println(printLengthOftheLL(head));
 
-        traverseLinkedList(head);
+        // traverseLinkedList(head);
+
+        System.out.println(searchInLinkedList(head, 51));
+
+        int[] arr1 = { 2, 7, 9, 11 };
+        int target = 13;
+
+        int[] result = twoSum(arr1, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+
     }
 
 }
