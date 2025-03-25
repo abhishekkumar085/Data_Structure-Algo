e9e40ff6fae0f528a01b0bef535aec3f2454681b
abhishekkumar085 <abhishek950820@gmail.com>
2025-03-25 23:05:01 +0530

    Revise Anagram using Hashmap

---
+++ Revise/Ana.java
@@ -0,0 +1,40 @@
+
+import java.util.HashMap;
+
+public class Ana {
+
+    private static boolean anagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> ana = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            ana.put(ch, ana.getOrDefault(ch, 0) + 1);
+
+        }
+
+        // decrease the count for t that is str2;
+
+        for (char ch : t.toCharArray()) {
+            if (!ana.containsKey(ch)) {
+                return false;
+            }
+            ana.put(ch, ana.get(ch) - 1);
+            if (ana.get(ch) == 0) {
+                ana.remove(ch);
+            }
+        }
+
+        return ana.isEmpty();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(anagram("listen", "sislent"));
+
+    }
+
+}
